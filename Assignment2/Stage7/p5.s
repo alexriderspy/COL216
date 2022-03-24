28062c2fe284120f3e05df62307363c7edb33f4f
alexriderspy <sreemantidey1234@gmail.com>
2022-03-24 22:13:27 +0530

    done

--- Assignment2/Stage7/p5.s
+++ Assignment2/Stage7/p5.s
@@ -2,6 +2,7 @@
     mov r0,#2
     mov r1,#5
     mov r2,#10
-    smlal r3,r2,r1,r0
-    umlal r3,r2,r1,r0
+    mov r3,#0
+    smlal r2,r3,r1,r0 @ r3,r2 = r0 * r1 + r3,r2
+    umlal r2,r3,r1,r0 @ r3,r2 = r0 * r1 + r3,r2
 .end
--- Assignment2/Stage7/p6.s
+++ Assignment2/Stage7/p6.s
@@ -1,8 +1,11 @@
 .text
-    mov r0, #-2
-    mov r1,#2
-    mov r3,#0
-    str r0,[r3]
-    ldrh r2,[r3],#2
+    mov r0, #0
+    mov r1, #0
+	mov r3, #0
+Loop1: add r0, r0, r1
+    add r1, r1, #1
+	str r0,[r3]
+	ldr r0,[r3]
+    cmn r1, #-3
+    bne Loop1
 .end
-
