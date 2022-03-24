49a970cfcd46f6d68878ada6dc733b09bab9cdea
alexriderspy <sreemantidey1234@gmail.com>
2022-03-25 01:06:12 +0530

    submitted

--- Assignment2/Stage7/p1.s
+++ Assignment2/Stage7/p1.s
@@ -1,5 +1,5 @@
 .text
-    mov r0, #1
+    mov r0, #3
     mov r1, #2
     mul r3,r0,r1
 .end
--- Assignment2/Stage7/p3.s
+++ Assignment2/Stage7/p3.s
@@ -1,6 +1,6 @@
 .text
     mov r0,#2
     mov r1,#5
-    smull r3,r2,r1,r0 @r2,r3 = r0 * r1
+    smull r3,r2,r1,r0
     umull r3,r2,r1,r0
 .end
