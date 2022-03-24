c857f55c9b3ae5ed4d227b16d09d28d8c2648e45
alexriderspy <sreemantidey1234@gmail.com>
2022-03-24 23:31:47 +0530

    done

--- Assignment2/Stage7/p4.s
+++ Assignment2/Stage7/p4.s
@@ -1,5 +1,5 @@
 .text
     mov r0,#2
     mov r1,#-5
-    smull r3,r2,r1,r0
+    smulls r3,r2,r1,r0
 .end
--- Assignment2/Stage7/p6.s
+++ Assignment2/Stage7/p6.s
@@ -6,6 +6,7 @@ Loop1: add r0, r0, r1
     add r1, r1, #1
 	str r0,[r3]
 	ldr r0,[r3]
+    muls r4,r0,r1
     cmn r1, #-3
     bne Loop1
 .end
